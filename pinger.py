54755539134480524f7dc92ad0634b14dd136f93
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2023-05-04 23:56:13 -0700

    create utils directory

--- pinger.py
+++ pinger.py
@@ -8,42 +8,6 @@ from threading import Thread, active_count
 
 
 
-class Config:
-    def __init__(self):
-        self.links = dict()
-        self.interval = 1800
-
-    def add_product(self):
-        while True:
-            new_product_url = input("Enter URL to product (enter 'x' when finished):    ")
-            if (new_product == 'x'):
-                break
-            name = get_product_name(new_product_url)
-            self.links[name] = new_product_url
-        update_config(self.__dict__)
-
-    def load_config(self):
-        try:
-            config_object = open('config.json', 'r')
-        except FileNotFoundError:
-            print ("No config file found. Creating new config...")
-            config_object = open('config.json', 'x')
-            config_object.write(dumps(self.__dict__, indent=2))
-            config_object.close()
-        except Exception:
-            print ("Error with loading configuration")
-        finally:
-            json_object = load(config_object)
-            self.links = json_object("links")
-            self.interval = json_object("interval")
-            config_object.close()
-
-    def update_config(self):
-        new_file = open('config.json', 'w')
-        json_object = dumps(self.__dict__, indent=2)
-        new_file.write(json_object)
-        new_file.close()
-
 def play_sound(sound_file):
     try:
         audio_object = WaveObject.from_wave_file(f"./sounds/{sound_file}")
