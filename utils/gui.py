123469fa492ec44d839a3b271c51c052ca8b9eee
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2023-05-16 22:55:24 -0700

    GUI work

--- utils/gui.py
+++ utils/gui.py
@@ -5,3 +5,12 @@ class GUI:
     def __init__(self):
         self.root = tk.TK()
         self.entry1 = tk.Entry(self)
+
+root = tk.Tk()
+root.geometry("600x600")
+root.title("Techping")
+
+heading = tk.Label(root, text="Techping", font=('Courier New', 16))
+heading.pack(pady=10)
+
+root.mainloop()
