11a81afa38f1b13fb82573aeb169e0d54828ad99
NinjaOwl - Darr H <39216431+DivoHub@users.noreply.github.com>
2024-03-07 23:44:17 -0800

    revert to simpleaudio

--- utils/play_sound.py
+++ utils/play_sound.py
@@ -1,15 +1,15 @@
-import simpleaudio
+from simpleaudio import WaveObject
 
-#play notification sound
+#takes sound file with extension as argument then plays corresponding sound
 def play_sound(sound_file):
-        try:
-            audio_object = WaveObject.from_wave_file(f"./sounds/{sound_file}")
-            play = audio_object.play()
-            play.wait_done()
-            play.stop()
-        except FileNotFoundError:
-            print(f"{Colour().error} {sound_file} file not found. {Colour().default}")
-        except Exception:
-            print (f"{Colour().error} Error with playing notification audio. {Colour().default}")
-        finally:
-            return
+    try:
+        audio_object = WaveObject.from_wave_file(f"./sounds/{sound_file}")
+        play = audio_object.play()
+        play.wait_done()
+        play.stop()
+    except FileNotFoundError:
+        print(f"{sound_file} file not found.")
+    except Exception:
+        print("Error with playing notification audio.")
+    finally:
+        return
